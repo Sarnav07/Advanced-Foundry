fc94a641f51cd40c46217b3ad2b349f6e0ce1ff8
Sarnav07 <sarnavkansal81@gmail.com>
2026-03-05 10:19:06 +0000

    Removed libs and other irrelevant stuff

--- DAO/src/TimeLock.sol
+++ DAO/src/TimeLock.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.30;
 import {TimelockController} from "../lib/openzeppelin-contracts/contracts/governance/TimelockController.sol";
 
 contract TimeLock is TimelockController {
-    // minDelay is how long you have to wait before executing
-    // proposers is the list of addresses that can propose
-    // executors is the list of addresses that can execute
     constructor(
         uint256 minDelay,
         address[] memory proposers,
